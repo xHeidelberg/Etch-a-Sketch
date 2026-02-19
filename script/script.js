4478359db6070daed23f56be7765a85a7f263140
Ded-Laptop <167631120+xHeidelberg@users.noreply.github.com>
2026-02-19 13:18:00 +0800

    add UI interface, add roleback

--- script/script.js
+++ script/script.js
@@ -1,1 +1,9 @@
 // Initialization
+
+let checkColor = document.querySelector('#favcolor');
+
+
+// correction only on mouse up or click outside then the rgb will be get
+checkColor.oninput = e => {
+    console.log(e.target.value);
+}
