c5cdf58720af3c25bdc4bb44dc187958965eb788
Ded-Laptop <167631120+xHeidelberg@users.noreply.github.com>
2026-02-24 00:11:52 +0800

    added new image, UI Corrected, removed
    script improved remove clutter
    script added and modified for DOM div
    css modified for DOM and animation
    html modified structure
    new image added for background

--- script/script.js
+++ script/script.js
@@ -1,9 +1,70 @@
-// Initialization
+const container = document.querySelector('#paintContainer');
+const setGridBtn = document.querySelector('#setGrid');
+const setRainbowBtn = document.querySelector('#setRainbow');
+const setBlackBtn = document.querySelector('#setBlack');
+const clearBtn = document.querySelector('#clearCanvas');
+let defaultGrid = 16;
+actionCall(defaultGrid);
 
-let checkColor = document.querySelector('#favcolor');
 
 
-// correction only on mouse up or click outside then the rgb will be get
-checkColor.oninput = e => {
-    console.log(e.target.value);
+setGridBtn.addEventListener('click', () => {
+    let inputGrid = prompt('Set Grid Default is 16x16');
+    if (parseInt(inputGrid) < 4 || parseInt(inputGrid) > 100) {
+        alert('Minimum of 4 \nMaximum of 100');
+        return;
+    }
+    if (!inputGrid || inputGrid.trim() == "" || !parseInt(inputGrid)) return console.log('capture cancel/null/space/letters');
+
+    //clean as Int then pass to function for grid creation
+    const gridView = document.querySelector('#toolContainer span');
+    gridView.textContent = `${inputGrid + 'x' + inputGrid}`;
+    actionCall(parseInt(inputGrid));
+})
+
+function randomRGB() {
+    const r = Math.floor(Math.random() * 226);
+    const g = Math.floor(Math.random() * 226);
+    const b = Math.floor(Math.random() * 226);
+
+    const shuffle = `rgb(${r}, ${g}, ${b})`;
+    return shuffle;
 }
+
+function actionCall(manyGrid) {
+    container.innerHTML = "";
+    let style = `background-color: white; border: 1px solid black; margin: 0; padding: 0; width: ${450 / manyGrid}; height: ${550 / manyGrid}; display: inline-block;`;
+    const doubleGrid = manyGrid ** 2; // Exponent or times (2)
+    for (let createLoop = 0; createLoop < doubleGrid; createLoop++) {
+        // console.log(createLoop);
+        const pixelCreate = document.createElement('div');
+        pixelCreate.style.cssText = style;
+        pixelCreate.addEventListener('mouseover', () => {
+            pixelCreate.style.backgroundColor = 'black';
+        });
+
+        // rainbow
+        setRainbowBtn.addEventListener('click', () => {
+            pixelCreate.addEventListener('mouseover', () => {
+                pixelCreate.style.backgroundColor = randomRGB();
+            })
+        });
+
+        // black
+        setBlackBtn.addEventListener('click', () => {
+            pixelCreate.addEventListener('mouseover', () => {
+                pixelCreate.style.backgroundColor = 'black';
+            })
+        });
+
+        // clear
+        clearBtn.addEventListener('click', () => {
+            let cleaners = document.querySelectorAll('#paintContainer div');
+            cleaners.forEach(cleaner => {
+                cleaner.style.backgroundColor = 'white';
+            })
+        });
+
+        container.appendChild(pixelCreate);
+    }
+};
